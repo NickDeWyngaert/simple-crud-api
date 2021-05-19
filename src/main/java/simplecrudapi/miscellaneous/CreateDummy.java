6c2944f3c0b073bdb33a3d9ab34dc889f49acd1f
NickDeWyngaert <43341508+NickDeWyngaert@users.noreply.github.com>
2021-05-19 13:49:02 +0200

    More dummy movies

--- src/main/java/simplecrudapi/miscellaneous/CreateDummy.java
+++ src/main/java/simplecrudapi/miscellaneous/CreateDummy.java
@@ -53,6 +53,51 @@ public abstract class CreateDummy {
                 "https://m.media-amazon.com/images/M/MV5BMTg3ODY5ODI1NF5BMl5BanBnXkFtZTgwMTkxNTYxMTE@._V1_UX182_CR0,0,182,268_AL_.jpg",
                 "Jim, Oz, Finch and Kevin are four friends who make a pact that before they graduate they will all lose their virginity. The hard job now is how to reach that goal by prom night. Whilst Oz begins singing to grab attention and Kevin tries to persuade his girlfriend, Finch tries any easy route of spreading rumors and Jim fails miserably. Whether it is being caught on top of a pie or on the Internet, Jim always ends up with his trusty sex advice from his father. Will they achieve their goal of getting laid by prom night? Or will they learn something much different?"
         ));
+        movies.add(new Movie(
+                "I Am Legend",
+                "action",
+                "Francis Lawrence",
+                "2007-12-19",
+                101,
+                "https://m.media-amazon.com/images/M/MV5BYTE1ZTBlYzgtNmMyNS00ZTQ2LWE4NjEtZjUxNDJkNTg2MzlhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                "Robert Neville (Will Smith) is a scientist who was unable to stop the spread of the terrible virus that was incurable and man-made in this post-apocalyptic action thriller. Immune, Neville is now the last human survivor in what is left of New York City and perhaps the world. For three years, Neville has faithfully sent out daily radio messages, desperate to find any other survivors who might be out there. But he is not alone. Mutant victims of the plague - the Infected - lurk in the shadows - watching Neville's every move - waiting for him to make a fatal mistake. Perhaps mankind's last, best hope, Neville is driven by only one remaining mission: to find a way to reverse the effects of the virus using his own immune blood. But he knows he is outnumbered - and quickly running out of time."
+        ));
+        movies.add(new Movie(
+                "The Mechanic",
+                "crime",
+                "Simon West",
+                "2011-04-06",
+                93,
+                "https://m.media-amazon.com/images/M/MV5BMjEyMjk1NjI1M15BMl5BanBnXkFtZTcwODcyNjAxNA@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                "Arthur Bishop (Jason Statham) is a 'mechanic' - an elite assassin with a strict code and unique talent for cleanly eliminating targets. It's a job that requires professional perfection and total detachment, and Bishop is the best in the business. But when his mentor and close friend Harry (Donald Sutherland) is murdered, Bishop is anything but detached. His next assignment is self-imposed - he wants those responsible dead. His mission grows complicated when Harry's son Steve (Ben Foster) approaches him with the same vengeful goal and a determination to learn Bishop's trade. Bishop has always acted alone but he can't turn his back on Harry's son. A methodical hit man takes an impulsive student deep into his world and a deadly partnership is born. But while in pursuit of their ultimate mark, deceptions threaten to surface and those hired to fix problems become problems themselves."
+        ));
+        movies.add(new Movie(
+                "The Transporter",
+                "action",
+                "Louis Leterrier Corey Yuen",
+                "2002-11-06",
+                92,
+                "https://m.media-amazon.com/images/M/MV5BMTk2NDc2MDAxN15BMl5BanBnXkFtZTYwNDc1NDY2._V1_UY268_CR2,0,182,268_AL_.jpg",
+                "Ex-Special Forces operator Frank Martin lives what seems to be a quiet life along the French Mediterranean, hiring himself out as a mercenary \"transporter\" who moves goods--human or otherwise--from one place to another. No questions asked. Carrying out mysterious and sometimes dangerous tasks in his tricked-out BMW, Frank adheres to a strict set of rules, which he never breaks. Rule One: Never change the deal. Rule Two: No names--Frank doesn't want to know whom he's working for, or what he's transporting. Rule Three: never look in the package. Frank's newest transport seems no different from the countless ones he's done in the past. He's been hired by an American known only as \"Wall Street\" to make a delivery; but when Frank stops along the route, he notices his package is moving. Violating Rule Three, Frank looks inside the bag, finding its contents to be a beautiful, gagged woman."
+        ));
+        movies.add(new Movie(
+                "Iron Man",
+                "action",
+                "Jon Favreau",
+                "2008-04-30",
+                126,
+                "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                "Tony Stark. Genius, billionaire, playboy, philanthropist. Son of legendary inventor and weapons contractor Howard Stark. When Tony Stark is assigned to give a weapons presentation to an Iraqi unit led by Lt. Col. James Rhodes, he's given a ride on enemy lines. That ride ends badly when Stark's Humvee that he's riding in is attacked by enemy combatants. He survives - barely - with a chest full of shrapnel and a car battery attached to his heart. In order to survive he comes up with a way to miniaturize the battery and figures out that the battery can power something else. Thus Iron Man is born. He uses the primitive device to escape from the cave in Iraq. Once back home, he then begins work on perfecting the Iron Man suit. But the man who was put in charge of Stark Industries has plans of his own to take over Tony's technology for other matters."
+        ));
+        movies.add(new Movie(
+                "Men in Black II",
+                "adventure",
+                "Barry Sonnenfeld",
+                "2002-08-14",
+                88,
+                "https://m.media-amazon.com/images/M/MV5BMTMxNDA0NTMxMV5BMl5BanBnXkFtZTYwMDE2NzY2._V1_UX182_CR0,0,182,268_AL_.jpg",
+                "Kay and Jay reunite to provide our best, last and only line of defense against a sinister seductress who levels the toughest challenge yet to the MIBs untarnished mission statement: protecting the earth from the scum of the universe. It's been four years since the alien-seeking agents averted an intergalactic disaster of epic proportions. And now it's a race against the clock as Jay must convince Kay-who not only has absolutely no memory of his time spent with the Men In Black, but is also the only living person left with the expertise to save the galaxy-to reunite with the MIB before the earth submits to ultimate destruction."
+        ));
         return movies;
     }
 
